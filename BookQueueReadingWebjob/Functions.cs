f73d5a8fe596f359b02d46c16a327cf2cca78d72
shailendra-g <shail27.g@gmail.com>
2018-10-10 21:35:07 -0500

    Add project files.

---
+++ BookQueueReadingWebjob/Functions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.ServiceBus.Messaging;
+using Newtonsoft.Json;
+
+namespace BookQueueReadingWebjob
+{
+
+    public class Books
+    {
+        //public int BookId { get; set; }
+        public string BookName { get; set; }
+        public string AuthorName { get; set; }
+        public Nullable<decimal> Price { get; set; }
+        public Nullable<System.DateTime> PublishDate { get; set; }
+    }
+
+    public class Functions
+    {
+       
+        public void ProcessQueueMessage([ServiceBusTrigger("%QueueName%")] BrokeredMessage brokeredMessage)
+        {
+            try
+            {
+                var bodyJson = new StreamReader(brokeredMessage.GetBody<Stream>(), Encoding.UTF8).ReadToEnd();
+                Books b = JsonConvert.DeserializeObject<Books>(bodyJson);
+
+                Console.WriteLine(bodyJson);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            brokeredMessage.Complete();
+        }
+    }
+}
---
+++ BookQueueReadingWebjob/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using System.Configuration;
+
+namespace BookQueueReadingWebjob
+{
+    // To learn more about Microsoft Azure WebJobs SDK, please see https://go.microsoft.com/fwlink/?LinkID=320976
+    class Program
+    {
+        // Please set the following connection strings in app.config for this WebJob to run:
+        // AzureWebJobsDashboard and AzureWebJobsStorage
+        static void Main()
+        {
+            var config = new JobHostConfiguration();
+
+            if (config.IsDevelopment)
+                config.UseDevelopmentSettings();
+           
+            config.UseServiceBus(new Microsoft.Azure.WebJobs.ServiceBus.ServiceBusConfiguration()
+            {
+                MessageOptions = new Microsoft.ServiceBus.Messaging.OnMessageOptions()                
+            });
+
+            var host = new JobHost(config);
+
+            // The following code ensures that the WebJob will be running continuously
+            host.RunAndBlock();
+        }
+    }
+}
